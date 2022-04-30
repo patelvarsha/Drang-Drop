c7fa1fa5982a30f77a2cf1d58919ea7ac7bc9f3b
Varsha <patelvarsha59848@gmail.com>
2022-04-30 10:41:58 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+	import { render } from 'react-dom'
+	import {Container} from './Componets/Container'
+	import { DndProvider } from 'react-dnd'
+	import { HTML5Backend } from 'react-dnd-html5-backend'
+
+	function App() {
+		return (
+			<div className="App">
+				<DndProvider backend={HTML5Backend}>
+					<Container />
+				</DndProvider>
+			</div>
+		)
+	}
+
+	const rootElement = document.getElementById('root')
+	render(<App />, rootElement)
